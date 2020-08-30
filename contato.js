4c508a767cc801380ac34cbf39de4d4ddbb4c390
RafaelMiranda21 <38355955+RafaelMiranda21@users.noreply.github.com>
2020-08-30 20:47:11 -0300

    Add files via upload

---
+++ contato.js
@@ -0,0 +1,17 @@
+document.querySelector(".trabalhe-conosco").addEventListener("click", () =>
+document.querySelector(".container").classList.toggle("show-form")
+);
+
+document.querySelector(".fechar-form").addEventListener("click", () =>
+document.querySelector(".container").classList.toggle("show-form")
+);
+
+
+document.querySelector(".fale-conosco").addEventListener("click", () =>
+document.querySelector(".container").classList.toggle("show-contato")
+);
+
+document.querySelector(".fechar-chat").addEventListener("click", () =>
+document.querySelector(".container").classList.toggle("show-contato")
+);
+
---
+++ slide.js
@@ -0,0 +1,55 @@
+let slides;
+let pontoIndicador;
+let imgAtual;
+let tempoTroca;
+let Intervalo;
+
+function carregaImg(img){
+   for(let i=0; i < slides.length; i++){
+       slides[i].style.display = "none";
+       pontoIndicador[i].className = pontoIndicador[i].className.replace(" ativo", "");
+   }
+    
+   slides[img].style.display = "block";
+   pontoIndicador[img].className += " ativo";
+}
+
+function troca(n){
+    tempoTroca = 0;
+    imgAtual+=n;
+    console.log(imgAtual);
+    if(imgAtual > slides.length-1){
+        imgAtual = 0;
+    }else if(imgAtual < 0){
+        imgAtual = slides.length-1;
+    }
+
+    carregaImg(imgAtual);
+}
+
+function tempo(){
+    tempoTroca++;
+    if(tempoTroca >= Intervalo){
+        tempoTroca = 0;
+        troca(1);
+    }
+    window.requestAnimationFrame(tempo);
+}
+
+function Indicador(n){
+
+   imgAtual = n;
+   carregaImg(imgAtual);
+}
+
+ function inicia(){
+         slides = document.getElementsByClassName("slide");
+         pontoIndicador = document.getElementsByClassName("ponto-indicador-slide");
+         imgAtual = 0 ;
+         tempoTroca = 0;
+         Intervalo = 200;
+         carregaImg(imgAtual);
+         tempo()
+     }
+  
+     window.addEventListener("load",inicia);
