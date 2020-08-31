df002bfd444893985220f0191cfa2661bd59266c
RafaelMiranda21 <38355955+RafaelMiranda21@users.noreply.github.com>
2020-08-31 12:36:55 -0300

    Update script.js

--- script.js
+++ script.js
@@ -2,3 +2,8 @@
      document.querySelector(".container").classList.toggle("show-menu")
  );
 
+
+ function fecharSidebar(){
+    let Sidebar =  document.querySelector(".container");
+    Sidebar.className = Sidebar.className.replace(" show-menu", "");
+ }
