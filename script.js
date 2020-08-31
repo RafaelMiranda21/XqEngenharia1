f23ecb963c6e10719d78ad2721cdb1ab2fb82259
RafaelMiranda21 <38355955+RafaelMiranda21@users.noreply.github.com>
2020-08-31 12:38:42 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
- document.querySelector(".hamburguer").addEventListener("click", () =>
-     document.querySelector(".container").classList.toggle("show-menu")
+ 
+ let Hamburguer = document.querySelector(".hamburguer");
+ let Sidebar =  document.querySelector(".container");
+
+
+ Hamburguer.addEventListener("click", () =>
+    Sidebar.classList.toggle("show-menu")
  );
 
 
  function fecharSidebar(){
-    let Sidebar =  document.querySelector(".container");
     Sidebar.className = Sidebar.className.replace(" show-menu", "");
  }
